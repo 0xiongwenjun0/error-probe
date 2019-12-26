f81a22cea0ff9cfae1a39afdc239113307c58d68
xiongwenjun@100tal.com <xiongwenjun@100tal.com>
2019-12-26 16:28:22 +0800

    异步加载模块

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import { config, setConfig } from "./config"
-import {
-    _handleWindowError,
-    _handleRejectPromise,
-    _handleResourceError,
-    _handleFetchError,
-    _handleAjaxError,
-    _handleConsoleError,
-    _handleConsoleWarnning,
-    _handleVueError,
-    _handleVueWarn,
-    _ThrowError,
-    _ThrowWarn,
-    _ThrowInfo,
-} from "./hooks"
+// import {
+//     _handleWindowError,
+//     _handleRejectPromise,
+//     _handleResourceError,
+//     _handleFetchError,
+//     _handleAjaxError,
+//     _handleConsoleError,
+//     _handleConsoleWarnning,
+//     _handleVueError,
+//     _handleVueWarn,
+//     _ThrowError,
+//     _ThrowWarn,
+//     _ThrowInfo,
+// } from "./hooks"
 // import  _startRecord  from "./record"
 
 import { _window } from "./redux"
@@ -31,44 +31,53 @@ function start(options, extend) {
     // 开始录制
     if (config.record) {
         // console.log('=====开始录制轨迹======');
-        import("./record").then(res=>{
-            res.default(_window)
+        import("./record").then(result => {
+            result.default(_window)
         })
     }
     // 处理过滤器
     if (config.jsError) {
-        _handleWindowError(_window, config);
-    }
-    if (config.jsError) {
-        _handleRejectPromise(_window, config);
+        import("./hooks/js").then(result => {
+            result._handleWindowError(_window, config);
+            result._handleRejectPromise(_window, config);
+            if (config.Vue || config.vue) {
+                result._handleVueError(_window, config);
+                if (!config.closeWarn)
+                    result._handleVueWarn(_window, config)
+            }
+        })
     }
     if (config.resourceError && addEventListener) {
-        _handleResourceError(_window, config);
+        import("./hooks/resource").then(result => {
+            result._handleResourceError(_window, config);
+        })
     }
     if (config.ajaxError) {
-        _handleFetchError(_window, config);
-        _handleAjaxError(_window, config);
+        import("./hooks/network").then(result => {
+            result._handleFetchError(_window, config);
+            result._handleAjaxError(_window, config);
+        })
     }
-    if (config.consoleError) {
-        _handleConsoleError(_window, config);
-        if (!config.closeWarn)
-            _handleConsoleWarnning(_window, config);
+    if (config.custom || config.consoleError) {
+        import("./hooks/log").then(result => {
+            if (config.custom) {
+                _window.fireLog = {
+                    error: result._ThrowError(config),
+                    warn: result._ThrowWarn(config),
+                    info: result._ThrowInfo(config)
+                }
+            }
+            if (config.consoleError) {
+                result._handleConsoleError(_window, config);
+                if (!config.closeWarn)
+                    result._handleConsoleWarnning(_window, config);
+            }
+        })
     }
-    if (config.Vue || config.vue) {
-        _handleVueError(_window, config);
-        if (!config.closeWarn)
-            _handleVueWarn(_window, config)
-    }
-    if (config.custom) {
-        _window.fireLog = {
-            error: _ThrowError(config),
-            warn: _ThrowWarn(config),
-            info: _ThrowInfo(config)
-        }
-    }
-    _window.addEventListener("beforeunload", function () {
-        config.sendWarn({}, true)
-    })
+    if (!config.closeWarn)
+        _window.addEventListener("beforeunload", function () {
+            config.sendWarn({}, true)
+        })
 }
 
 export default start
